31db293fa61d25cb7e6fc357a8377f0f90555a2f
Jonah Bedouch <jonah@bedouch.net>
2023-02-27 23:42:37 -0800

    Update home page and resume to reflect end of internship

--- src/lib/components/WorkExperience.svelte
+++ src/lib/components/WorkExperience.svelte
@@ -6,19 +6,19 @@
 	<h1 class="heading">Work Experience.</h1>
 
 	<WorkEntry
-		employer="Bosch USA"
-		title="Customer Engineering Intern - Nevonex"
-		timeRange="October 2022 - Present"
-		description="Redesign User Interface for Nevonex Features to maximize usability by improving layout and standardizing control structure. Modernize existing 
-    frontend using VueJS to improve efficiency, maintainability, and extensibility."
-	/>
-	<WorkEntry
 		employer="Leap XR, LLC."
 		title="Full Stack Developer"
 		timeRange="January 2021 - Present"
 		description="Develop a teacher dashboard used to view student data and as an aid in administering lessons and starting virtual experiences using VueJS and Tailwind CSS
     and implement websocket-based networking and a Node.js backend server to facilitate communication
     between Virtual Reality headsets, a Web Dashboard, and a MongoDB database."
+	/>
+	<WorkEntry
+		employer="Bosch USA"
+		title="Customer Engineering Intern - Nevonex"
+		timeRange="October 2022 - March 2022"
+		description="Redesign User Interface for Nevonex Features to maximize usability by improving layout and standardizing control structure. Modernize existing 
+    frontend using VueJS to improve efficiency, maintainability, and extensibility."
 	/>
 </div>
 
