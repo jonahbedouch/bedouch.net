98d218c018a7b347c4e5ee53dd86d2af55b0e4ee
Jonah Bedouch <jonah@bedouch.net>
2024-08-28 17:38:16 -0700

    Update Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        staleTimes: {
+            dynamic: 300,
+            static: 600,
+        }
+    }
+};
 
 export default withPlaiceholder(nextConfig);
