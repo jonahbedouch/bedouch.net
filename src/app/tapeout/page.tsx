730871deed0957017075269195b780b092cef9e5
Jonah Alex Bedouch <jonah@bedouch.net>
2026-01-22 23:51:18 -0800

    [tapeout] update to ee194

--- src/app/tapeout/page.tsx
+++ src/app/tapeout/page.tsx
@@ -5,7 +5,7 @@ export default function Tapeout() {
     <div className="w-full grid grid-cols-12">
         <main className="lg:py-sm px-sm py-md mt-3xs-xl col-span-12 bg-secondary-0 dark:bg-secondary-1000 overflow-hidden rounded-lg shadow-medium dark:shadow-d-medium ring-1 ring-secondary-1000 dark:ring-secondary-900 ring-opacity-5">
             <div className="flex flex-row">
-                <h1 className="text-3xl font-bold mr-4">Has Jim Taped Out Yet?</h1>
+                <h1 className="text-3xl font-bold mr-4">Has EE194 Taped Out Yet?</h1>
                 <h1 className="text-3xl font-bold text-red-600">No.</h1>
                 {/* <h1 className="text-3xl font-bold text-green-600">Tes.</h1> */}
             </div>
